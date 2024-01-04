1ad836f15b3ea21f15a9fd0d5e3d61b5752fa589
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 19:51:23 +0100

    remove optional in getCommentsByProductId

--- src/main/java/cz/cvut/fit/onlinestore/controller/CommentController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/CommentController.java
@@ -2,9 +2,7 @@ package cz.cvut.fit.onlinestore.controller;
 
 import cz.cvut.fit.onlinestore.dao.dto.CommentAddDTO;
 import cz.cvut.fit.onlinestore.dao.dto.CommentDescriptionDTO;
-import cz.cvut.fit.onlinestore.dao.dto.ProductDescriptionDTO;
 import cz.cvut.fit.onlinestore.service.CommentService;
-import cz.cvut.fit.onlinestore.service.ProductService;
 import cz.cvut.fit.onlinestore.service.exceptions.ProductWithThatIdDoesNotExistException;
 import cz.cvut.fit.onlinestore.service.exceptions.UserWithThatEmailDoesNotExistException;
 import lombok.RequiredArgsConstructor;
@@ -12,24 +10,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
     private final CommentService commentService;
-    private final ProductService productService;
 
     @GetMapping("/api/products/{id}/comments")
     public ResponseEntity<List<CommentDescriptionDTO>> getCommentsByProductId(@PathVariable Long id) {
-        Optional<ProductDescriptionDTO> product = productService.getProductById(id);
-
-        if (product.isPresent()) {
+        try {
             List<CommentDescriptionDTO> comments = commentService.getCommentsByProductId(id);
             return ResponseEntity.ok(comments);
-        } else {
-            return ResponseEntity.notFound().build();
+        } catch (ProductWithThatIdDoesNotExistException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
     }
 

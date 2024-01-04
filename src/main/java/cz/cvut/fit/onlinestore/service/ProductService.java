f599a8ea2e6189cacd39a21cf2518e9d5c3b9e82
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 20:09:21 +0100

    getAllProductsWithCategory into getAllProducts

--- src/main/java/cz/cvut/fit/onlinestore/service/ProductService.java
+++ src/main/java/cz/cvut/fit/onlinestore/service/ProductService.java
@@ -17,22 +17,11 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<ProductDescriptionDTO> getAllProducts() {
-        List<Tuple> products = productRepository.getAllProducts();
-        return products.stream()
-                .map(product -> new ProductDescriptionDTO(
-                        product.get(0, Long.class),
-                        product.get(1, String.class),
-                        product.get(2, String.class),
-                        product.get(3, Double.class),
-                        product.get(4, String.class),
-                        product.get(5, String.class)
-                ))
-                .collect(Collectors.toList());
-    }
+    public List<ProductDescriptionDTO> getAllProducts(String category) {
+        List<Tuple> products = (category != null && category.isEmpty())
+                ? productRepository.getAllProductsWithCategory(category)
+                : productRepository.getAllProducts();
 
-    public List<ProductDescriptionDTO> getAllProductsWithCategory(String category) {
-        List<Tuple> products = productRepository.getAllProductsWithCategory(category);
         return products.stream()
                 .map(product -> new ProductDescriptionDTO(
                         product.get(0, Long.class),

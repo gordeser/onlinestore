e9d7816db54e841159ccb849671b8a8feb6590fd
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:48:05 +0100

    add getAllProducts in ProductService

---
+++ src/main/java/cz/cvut/fit/onlinestore/service/ProductService.java
@@ -0,0 +1,31 @@
+package cz.cvut.fit.onlinestore.service;
+
+import cz.cvut.fit.onlinestore.dao.dto.ProductDescriptionDTO;
+import cz.cvut.fit.onlinestore.dao.repository.ProductRepository;
+import jakarta.persistence.Tuple;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductDescriptionDTO> getAllProducts() {
+        List<Tuple> products = productRepository.getAllProducts();
+        return products.stream()
+                .map(product -> new ProductDescriptionDTO(
+                        product.get(0, Long.class),
+                        product.get(1, String.class),
+                        product.get(2, String.class),
+                        product.get(3, Double.class),
+                        product.get(4, String.class),
+                        product.get(5, String.class)
+                ))
+                .collect(Collectors.toList());
+    }
+}

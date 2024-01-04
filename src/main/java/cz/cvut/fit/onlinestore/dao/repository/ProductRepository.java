9cd928eda03ae5426498a02c22337ecb61cdc9a9
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:57:07 +0100

    add getProductById query

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/ProductRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
@@ -14,4 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("select p.id, p.name, p.description, p.price, p.category, p.image from Product p where p.category = :category")
     List<Tuple> getAllProductsWithCategory(String category);
+
+    @Query("select p.name, p.description, p.price, p.category, p.image from Product p where p.id = :id")
+    Optional<Tuple> getProductById(Long id);
 }

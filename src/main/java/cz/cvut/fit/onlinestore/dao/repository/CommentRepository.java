3fcb3b67b25a6b6ec61b63ee183d91bc973a6046
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:24:06 +0100

    add getAllCommentsByProductId query

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/CommentRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/CommentRepository.java
@@ -2,6 +2,12 @@ package cz.cvut.fit.onlinestore.dao.repository;
 
 import cz.cvut.fit.onlinestore.dao.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select c from Comment c where c.product.id = :id")
+    List<Comment> getAllCommentsByProductId(Long id);
 }

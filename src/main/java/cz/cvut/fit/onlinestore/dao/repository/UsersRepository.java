37ac271fe31a72da4cf6902a3c11a172cce14316
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:35:33 +0100

    add findUserByEmail in UsersRepository

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/UsersRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/UsersRepository.java
@@ -2,6 +2,11 @@ package cz.cvut.fit.onlinestore.dao.repository;
 
 import cz.cvut.fit.onlinestore.dao.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findUserByEmail(String email);
 }

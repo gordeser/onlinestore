49910e3b9b7f9dd87393f4286449bdc3412d9276
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:57:40 +0100

    add getProductById in ProductController

--- src/main/java/cz/cvut/fit/onlinestore/controller/ProductController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/ProductController.java
@@ -3,9 +3,11 @@ package cz.cvut.fit.onlinestore.controller;
 import cz.cvut.fit.onlinestore.dao.dto.ProductDescriptionDTO;
 import cz.cvut.fit.onlinestore.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +23,16 @@ public class ProductController {
             return productService.getAllProductsWithCategory(category);
         } else {
             return productService.getAllProducts();
+        }
+    }
+
+    @GetMapping("/api/products/{id}")
+    public ResponseEntity<Optional<ProductDescriptionDTO>> getProductById(@PathVariable Long id) {
+        Optional<ProductDescriptionDTO> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 }

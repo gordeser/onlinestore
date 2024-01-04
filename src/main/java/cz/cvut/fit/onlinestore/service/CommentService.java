8b1484a9c0aff7a0a853d07ac897e2a1e474d801
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:28:53 +0100

    add addCommentByProductId method

--- src/main/java/cz/cvut/fit/onlinestore/service/CommentService.java
+++ src/main/java/cz/cvut/fit/onlinestore/service/CommentService.java
@@ -1,19 +1,28 @@
 package cz.cvut.fit.onlinestore.service;
 
+import cz.cvut.fit.onlinestore.dao.dto.CommentAddDTO;
 import cz.cvut.fit.onlinestore.dao.dto.CommentDescriptionDTO;
 import cz.cvut.fit.onlinestore.dao.dto.UsersCommentDTO;
 import cz.cvut.fit.onlinestore.dao.entity.Comment;
+import cz.cvut.fit.onlinestore.dao.entity.Product;
+import cz.cvut.fit.onlinestore.dao.entity.Users;
 import cz.cvut.fit.onlinestore.dao.repository.CommentRepository;
+import cz.cvut.fit.onlinestore.dao.repository.ProductRepository;
+import cz.cvut.fit.onlinestore.dao.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UsersRepository usersRepository;
+    private final ProductRepository productRepository;
 
     public List<CommentDescriptionDTO> getCommentsByProductId(Long id) {
         List<Comment> comments = commentRepository.getAllCommentsByProductId(id);
@@ -32,4 +41,31 @@ public class CommentService {
                 .collect(Collectors.toList());
     }
 
+    @Modifying
+    public Optional<CommentDescriptionDTO> addCommentByProductId(Long id, CommentAddDTO comment) {
+        Optional<Users> user = usersRepository.findByEmail(comment.email());
+        Optional<Product> product = productRepository.findById(id);
+
+        if (user.isPresent() && product.isPresent()) {
+            Comment newComment = new Comment();
+            newComment.setProduct(product.get());
+            newComment.setText(comment.text());
+            newComment.setUsers(user.get());
+            commentRepository.save(newComment);
+
+            return Optional.of(
+                    new CommentDescriptionDTO(
+                            newComment.getId(),
+                            newComment.getText(),
+                            newComment.getDate(),
+                            new UsersCommentDTO(
+                                    newComment.getUsers().getName(),
+                                    newComment.getUsers().getSurname(),
+                                    newComment.getUsers().getEmail()
+                            )
+                    ));
+        } else {
+            return Optional.empty();
+        }
+    }
 }

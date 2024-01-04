aaef3801a9faf3b8034102d36f56bb1121d1e39f
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 19:02:48 +0100

    add createOrder in OrdersController

---
+++ src/main/java/cz/cvut/fit/onlinestore/controller/OrdersController.java
@@ -0,0 +1,28 @@
+package cz.cvut.fit.onlinestore.controller;
+
+import cz.cvut.fit.onlinestore.dao.dto.OrderDescriptionDTO;
+import cz.cvut.fit.onlinestore.dao.entity.Orders;
+import cz.cvut.fit.onlinestore.service.OrdersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+public class OrdersController {
+    private final OrdersService ordersService;
+
+    @PostMapping("/api/orders")
+    public ResponseEntity<?> createOrder(@RequestBody OrderDescriptionDTO orderDescription) {
+        try {
+            Orders newOrder = ordersService.createOrder(orderDescription);
+            return ResponseEntity.ok(newOrder);
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body(e);
+        }
+    }
+}

bfc272110fc26cd1aabfd282cc29c2dbe9909747
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:21:36 +0100

    add generated id to product entity

--- src/main/java/cz/cvut/fit/onlinestore/dao/entity/Product.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/entity/Product.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class Product {
     @Id
     @Column(name = "id_product")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;

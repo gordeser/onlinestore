77f146145389c2a05f58cd6a661f98e360d8a5fa
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:44:36 +0100

    add getAllProducts query

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/ProductRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package cz.cvut.fit.onlinestore.dao.repository;
 
 import cz.cvut.fit.onlinestore.dao.entity.Product;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select p.id, p.name, p.description, p.price, p.category, p.image from Product p")
+    List<Tuple> getAllProducts();
 }

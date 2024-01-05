81b8940dfb631bc065898f79409666447d1d8ec5
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 18:41:01 +0100

    rename ProductCountDTO

--- src/main/java/cz/cvut/fit/onlinestore/dao/dto/OrderDescriptionDTO.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/dto/OrderDescriptionDTO.java
@@ -2,5 +2,5 @@ package cz.cvut.fit.onlinestore.dao.dto;
 
 import java.util.List;
 
-public record OrderDescriptionDTO(String userEmail, List<ProductCountDTO> orderProducts) {
+public record OrderDescriptionDTO(String userEmail, List<OrderProductsDTO> orderProducts) {
 }

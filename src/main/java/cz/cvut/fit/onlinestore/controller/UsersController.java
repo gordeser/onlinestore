847923936e1c9caae2c82de9cde3e156d0c79107
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 18:04:39 +0100

    add swagger docs to Users

--- src/main/java/cz/cvut/fit/onlinestore/controller/UsersController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/UsersController.java
@@ -4,19 +4,33 @@ import cz.cvut.fit.onlinestore.dao.dto.UsersLoginDTO;
 import cz.cvut.fit.onlinestore.dao.dto.UsersSignupDTO;
 import cz.cvut.fit.onlinestore.dao.entity.Users;
 import cz.cvut.fit.onlinestore.service.UsersService;
+import cz.cvut.fit.onlinestore.service.exceptions.UserWithThatEmailAlreadyExistsException;
 import cz.cvut.fit.onlinestore.service.exceptions.UserWithThatEmailDoesNotExistException;
 import cz.cvut.fit.onlinestore.service.exceptions.WrongPasswordException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Users", description = "Access to users")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
     private final UsersService usersService;
 
-    @PostMapping("/api/login")
+    @Operation(summary = "Log user in")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = UsersLoginDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Invalid credentials", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content(schema = @Schema())}),
+    })
+    @PostMapping("/api/users/login")
     public ResponseEntity<Users> getUserUsingCredentials(@RequestBody UsersLoginDTO userLogin) {
         try {
             Users user = usersService.authUser(userLogin);
@@ -30,7 +44,13 @@ public class UsersController {
         }
     }
 
-    @PostMapping("/api/signup")
+    @Operation(summary = "Create user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = {@Content(schema = @Schema(implementation = UsersSignupDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "User with that email already exists", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content(schema = @Schema())})
+    })
+    @PostMapping("/api/users")
     public ResponseEntity<Users> signupUser(@RequestBody UsersSignupDTO userSignup) {
         try {
             Users user = usersService.signupUser(userSignup);

4891101df292b619d77fc3dbb121ba4bf1b7af49
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 18:24:47 +0100

    add getOrderById in OrdersController

--- src/main/java/cz/cvut/fit/onlinestore/controller/OrdersController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/OrdersController.java
@@ -3,14 +3,12 @@ package cz.cvut.fit.onlinestore.controller;
 import cz.cvut.fit.onlinestore.dao.dto.OrderDescriptionDTO;
 import cz.cvut.fit.onlinestore.dao.entity.Orders;
 import cz.cvut.fit.onlinestore.service.OrdersService;
+import cz.cvut.fit.onlinestore.service.exceptions.OrderWithThatIdDoesNotExistException;
 import cz.cvut.fit.onlinestore.service.exceptions.TotalAmountOutOfRangeException;
 import cz.cvut.fit.onlinestore.service.exceptions.UserWithThatEmailDoesNotExistException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +17,20 @@ public class OrdersController {
     private final OrdersService ordersService;
 
     @PostMapping("/api/orders")
+    @GetMapping("/api/orders/{id}")
+    public ResponseEntity<Orders> getOrderById(@PathVariable Long id) {
+        try {
+            Orders order = ordersService.getOrderById(id);
+            return ResponseEntity.ok(order);
+        } catch (OrderWithThatIdDoesNotExistException e) {
+            System.out.println("ERROR: " + e);
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
     public ResponseEntity<Orders> createOrder(@RequestBody OrderDescriptionDTO orderDescription) {
         try {
             Orders newOrder = ordersService.createOrder(orderDescription);

c9c04d1f6aedd71c906a36a4f24729dd31b505c1
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 18:19:05 +0100

    add getOrdersById in OrdersRepository

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/OrdersRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/OrdersRepository.java
@@ -3,5 +3,8 @@ package cz.cvut.fit.onlinestore.dao.repository;
 import cz.cvut.fit.onlinestore.dao.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    Optional<Orders> getOrdersById(Long id);
 }

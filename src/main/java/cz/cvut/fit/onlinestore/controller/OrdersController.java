a782c48beb2e06104cad68888c58df9d1f55fd1a
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 18:47:27 +0100

    add swagger docs to Orders

--- src/main/java/cz/cvut/fit/onlinestore/controller/OrdersController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/OrdersController.java
@@ -6,19 +6,32 @@ import cz.cvut.fit.onlinestore.dao.entity.Orders;
 import cz.cvut.fit.onlinestore.service.OrdersService;
 import cz.cvut.fit.onlinestore.service.exceptions.OrderWithThatIdDoesNotExistException;
 import cz.cvut.fit.onlinestore.service.exceptions.UserWithThatEmailDoesNotExistException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Orders", description = "Access to store orders")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class OrdersController {
     private final OrdersService ordersService;
 
-    @PostMapping("/api/orders")
+    @Operation(summary = "Get order by its id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Orders.class))}),
+            @ApiResponse(responseCode = "404", description = "Order with that ID does not exist", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(schema = @Schema())})
+    })
     @GetMapping("/api/orders/{id}")
     public ResponseEntity<Orders> getOrderById(@PathVariable Long id) {
         try {
@@ -33,6 +46,13 @@ public class OrdersController {
         }
     }
 
+    @Operation(summary = "Create new order")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Orders.class))}),
+            @ApiResponse(responseCode = "404", description = "User with that email does not exist", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(schema = @Schema())})
+    })
+    @PutMapping("/api/orders")
     public ResponseEntity<Orders> createOrder(@RequestBody OrderDescriptionDTO orderDescription) {
         try {
             Orders newOrder = ordersService.createOrder(orderDescription);
@@ -45,6 +65,13 @@ public class OrdersController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
+    @Operation(summary = "Get orders by user's email")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Orders.class)))}),
+            @ApiResponse(responseCode = "404", description = "User with that email does not exist", content = { @Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = { @Content(schema = @Schema())}),
+    })
     @PostMapping("/api/orders")
     public ResponseEntity<List<Orders>> getUserOrders(@RequestBody UsersEmailDTO user) {
         try {

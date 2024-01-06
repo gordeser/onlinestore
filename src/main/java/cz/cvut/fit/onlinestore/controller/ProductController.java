80b82c64645c551150cd18228ba8cb30891deebf
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-06 14:50:03 +0100

    add others CRUD operations on Product

--- src/main/java/cz/cvut/fit/onlinestore/controller/ProductController.java
+++ src/main/java/cz/cvut/fit/onlinestore/controller/ProductController.java
@@ -1,6 +1,8 @@
 package cz.cvut.fit.onlinestore.controller;
 
+import cz.cvut.fit.onlinestore.dao.dto.ProductAddDTO;
 import cz.cvut.fit.onlinestore.dao.dto.ProductDescriptionDTO;
+import cz.cvut.fit.onlinestore.dao.entity.Product;
 import cz.cvut.fit.onlinestore.service.ProductService;
 import cz.cvut.fit.onlinestore.service.exceptions.ProductWithThatIdDoesNotExistException;
 import io.swagger.v3.oas.annotations.Operation;
@@ -54,6 +56,38 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         } catch (Exception e) {
             System.out.println("ERROR: " + e);
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("/api/product")
+    public ResponseEntity<Product> createProduct(@RequestBody ProductAddDTO product) {
+        try {
+            return ResponseEntity.ok(productService.createProduct(product));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @DeleteMapping("/api/product/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable Long id) {
+        try {
+            productService.deleteProductById(id);
+            return ResponseEntity.noContent().build();
+        } catch (ProductWithThatIdDoesNotExistException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PutMapping("/api/proudct/{id}")
+    public ResponseEntity<ProductDescriptionDTO> updateProduct(@PathVariable Long id, @RequestBody ProductDescriptionDTO productUpdate) {
+        try {
+            return ResponseEntity.ok(productService.updateProductById(id, productUpdate));
+        } catch (ProductWithThatIdDoesNotExistException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
     }
--- src/main/java/cz/cvut/fit/onlinestore/service/ProductService.java
+++ src/main/java/cz/cvut/fit/onlinestore/service/ProductService.java
@@ -1,9 +1,12 @@
 package cz.cvut.fit.onlinestore.service;
 
+import cz.cvut.fit.onlinestore.dao.dto.ProductAddDTO;
 import cz.cvut.fit.onlinestore.dao.dto.ProductDescriptionDTO;
+import cz.cvut.fit.onlinestore.dao.entity.Product;
 import cz.cvut.fit.onlinestore.dao.repository.ProductRepository;
 import cz.cvut.fit.onlinestore.service.exceptions.ProductWithThatIdDoesNotExistException;
 import jakarta.persistence.Tuple;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -48,5 +51,41 @@ public class ProductService {
         String image = product.get().get(4, String.class);
 
         return new ProductDescriptionDTO(id, name, description, price, category, image);
+    }
+
+    public Product createProduct(ProductAddDTO product) {
+        Product newProduct = new Product();
+        newProduct.setName(product.name());
+        newProduct.setDescription(product.description());
+        newProduct.setPrice(product.price());
+        newProduct.setCategory(product.category());
+        newProduct.setImage(product.image());
+        return productRepository.save(newProduct);
+    }
+
+    @Transactional
+    public void deleteProductById(Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+        } else {
+            throw new ProductWithThatIdDoesNotExistException();
+        }
+    }
+
+    public ProductDescriptionDTO updateProductById(Long id, ProductDescriptionDTO productUpdate) {
+        int updatedCount = productRepository.updateProduct(
+                id,
+                productUpdate.name(),
+                productUpdate.description(),
+                productUpdate.price(),
+                productUpdate.category(),
+                productUpdate.image()
+        );
+
+        if (updatedCount == 0) {
+            throw new ProductWithThatIdDoesNotExistException();
+        }
+
+        return productUpdate;
     }
 }

e1a93bfa5cee747f86576461e7e917555d4106d9
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-06 15:20:07 +0100

    add update comment query to repository

--- src/main/java/cz/cvut/fit/onlinestore/dao/repository/CommentRepository.java
+++ src/main/java/cz/cvut/fit/onlinestore/dao/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package cz.cvut.fit.onlinestore.dao.repository;
 
 import cz.cvut.fit.onlinestore.dao.entity.Comment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -13,4 +16,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c where c.product.id = :id")
     List<Comment> getAllCommentsByProductId(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Comment c SET c.text = :text, c.date = :date WHERE c.id = :id")
+    int updateComment(Long id, String text, LocalDateTime date);
 }
